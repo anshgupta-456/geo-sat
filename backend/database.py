22ef2519d4418f6f850dabd57b378f1c65dbb027
Ansh Gupta <anshgupta456ansh@ece.du.ac.in>
2026-03-05 21:49:05 +0530

    caps error

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,7 @@ SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin123@127.0.0.1:5434/geosat"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+Base = declarative_base()
 def get_db():
     db = SessionLocal()
     try:
